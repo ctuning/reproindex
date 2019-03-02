9bd45c25bf1f43808fd6ac1d4a8682c478aaf194
Grigori Fursin <Grigori.Fursin@cTuning.org>
2019-03-02 08:33:55 +0000

    added modules for indexing

---
+++ module/component/module.py
@@ -0,0 +1,156 @@
+#
+# Collective Knowledge (indexing reusable research components)
+#
+# 
+# 
+#
+# Developer: 
+#
+
+cfg={}  # Will be updated by CK (meta description of this module)
+work={} # Will be updated by CK (temporal data)
+ck=None # Will be updated by CK (initialized CK kernel) 
+
+# Local settings
+
+##############################################################################
+# Initialize module
+
+def init(i):
+    """
+
+    Input:  {}
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+    return {'return':0}
+
+##############################################################################
+# get index of components
+
+def get(i):
+    """
+    Input:  {
+              (web_vars_post)
+              (web_vars_get)
+              (web_vars_session)
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+
+              html - HTML
+            }
+
+    """
+
+    import copy
+    import math
+    import sys
+
+    wvg=i.get('web_vars_get',{})
+    if type(wvg)==list: wvg={}
+
+    wvp=i.get('web_vars_post',{})
+    if type(wvp)==list: wvp={}
+
+    wv=copy.deepcopy(wvp)
+    wv.update(wvg)
+
+    # Page length
+    length=wv.get('length','')
+    dlength='10'
+    if length=='': length=dlength
+    ilength=int(length)
+
+    # Page
+    page=wv.get('page','')
+    if page=='': page='1'
+    ipage=int(page)
+
+    # Check page name
+    page_name=i.get('page_name','')
+    if page_name=='':
+       return {'return':1, 'error':'page_name is empty'}
+
+    url=page_name
+
+    # Check search
+    q=wv.get('q','')
+
+    try:    from urllib.parse import urlencode
+    except: from urllib import urlencode # pragma: no cover
+
+    qq=urlencode({'q':q})
+    if sys.version_info[0]>2: qq=qq.encode('utf8')
+
+    url+='?'+qq
+
+    if ilength!=dlength:
+       url+='&length='+str(ilength)
+
+    # Search
+    ii={"action":"search",
+        "module_uoa":"repo",
+        "add_meta":"yes"}
+    r=ck.access(ii)
+    if r['return']>0: return r
+
+    lst=r['lst']
+    ep=r['elapsed_time']
+
+    llst=len(lst)
+
+    x=''
+    if llst==0 or llst>1: x='s'
+    h='<center>'+str(llst)+' result'+x+' ('+("%.3f" % float(ep))+' seconds)<br></center>\n'
+
+    # List
+    j1=(ipage-1)*ilength
+    j2=((ipage)*ilength)-1
+    if j2>=llst: j2=llst-1
+
+    for j in range(j1,j2+1):
+        jj=j+1
+        h+=str(jj)+'<br>\n'
+
+
+
+    # Get page index
+    tpages=int(math.ceil(float(llst)/float(ilength)))
+
+    if tpages>1:
+       h+='<p>Pages:&nbsp;&nbsp;&nbsp; '
+       h1=''
+       for p in range(0, tpages):
+           rp=p+1
+
+           url1=url
+           if ilength!=dlength:
+              url1+='&page='+str(rp)
+
+           x1='<a href="'+url1+'">'
+           x2='</a>'
+           if rp==ipage:
+              x1=''
+              x2=''
+
+           if h1!='': h1+=' '
+           h1+=x1+str(p+1)+x2
+
+       if (ipage+1)<=tpages:
+          url1=url
+          url1+='&page='+str(ipage+1)
+
+          h1+='&nbsp;&nbsp;&nbsp; <a href="'+url1+'">Next</a>\n'
+
+       h+=h1
+
+    return {'return':0, 'html':h}
